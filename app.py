9426536eeacb91fda11e34d421a81a2481ed127e
Anu <m2814aanu@gmail.com>
2026-05-17 17:29:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask,render_template,request
+import sqlite3
+app=Flask(__name__)
+conn=sqlite3.connect("users.db")
+cur=conn.cursor()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users(username TEXT , password TEXT)""")
+conn.commit()
+conn.close()
+@app.route("/")
+def home():
+    return render_template("login.html")
+@app.route("/register")
+def register_page():
+    return render_template("registration.html")
+@app.route("/register",methods=["POST"])
+def register():
+    username=request.form["username"]
+    email=request.form["email"]
+    password=request.form["password"]
+
+    conn=sqlite3.connect("users.db")
+    cur=conn.cursor()
+
+    cur.execute("INSERT INTO users VALUES(?,?)",(username,password))
+
+    conn.commit()
+    conn.close()
+
+    return f"Registered Successfully {username}"
+
+@app.route("/login",methods=["POST"])
+def login():
+    username=request.form["username"]
+    password=request.form["password"]
+
+    conn=sqlite3.connect("users.db")
+    cur=conn.cursor()
+
+    cur.execute("SELECT * FROM users WHERE username=? AND password=?",(username,password))
+
+    user=cur.fetchone()
+
+    conn.close()
+    if user:
+        return f"""
+            <script>
+            window.location.href='/home';</script>"""
+    else:
+        return "Invalid Login or Password"
+
+@app.route("/home")
+def homepage():
+    return render_template("home.html")
+app.run(debug=True)
