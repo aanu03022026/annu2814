58e730146e7953e317ba581174f4f9df1a7f97f9
Anu <m2814aanu@gmail.com>
2026-05-20 10:23:09 +0530

    Updated registration page and python code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request
 import sqlite3
-import re
 app=Flask(__name__)
 conn=sqlite3.connect("users.db")
 cur=conn.cursor()
@@ -11,27 +10,25 @@ conn.close()
 @app.route("/")
 def home():
     return render_template("login.html")
-
 @app.route("/register")
 def register_page():
     return render_template("registration.html")
-
-password=request.form["password"]
-if (len(password)>=8 and
-    re.search("[a-z]",password)and
-    re.search("[A-Z]",password)and
-    re.search("[0-9]",password)and
-    re.search("[@#$&*%!]",password)):
-
-    return "Strong Password.."
-else:
-    return "Password Must Contain Uppercase,Lowercase,Number and Special Character"
-    
 @app.route("/register",methods=["POST"])
 def register():
     username=request.form["username"]
     email=request.form["email"]
     password=request.form["password"]
+
+    password=request.form["password"]
+    if (len(password)>=8 and
+        re.search("[a-z]",password)and
+        re.search("[A-Z]",password)and
+        re.search("[0-9]",password)and
+        re.search("[@#$&*%!]",password)):
+
+        return "Strong Password.."
+    else:
+        return "Password Must Contain Uppercase,Lowercase,Number and Special Character"
 
     conn=sqlite3.connect("users.db")
     cur=conn.cursor()
